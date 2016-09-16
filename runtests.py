14a6a87dda21d3b61d6862bcf6ed240076d69f46
Scott Silliman <scott.t.silliman@gmail.com>
2016-09-16 00:57:03 -0400

    got rid of keepdb=True param to TestRunner as the only table is content-types

--- runtests.py
+++ runtests.py
@@ -12,7 +12,7 @@ def main():
     os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
     django.setup()
     TestRunner = get_runner(settings)
-    test_runner = TestRunner(keepdb=True)
+    test_runner = TestRunner()
     failures = test_runner.run_tests(["tests"])
     sys.exit(bool(failures))
 
