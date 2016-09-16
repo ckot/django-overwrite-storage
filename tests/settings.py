584e0c857a69bee3432aa3550bc659d68e1e448c
Scott Silliman <scott.t.silliman@gmail.com>
2016-09-16 01:00:16 -0400

    deleted previously commented out and thus unneeded midlewar classes. got rid of sqlite3 file-based test db, as the only table is 'content-types'

--- tests/settings.py
+++ tests/settings.py
@@ -6,29 +6,19 @@ BASE_DIR = os.path.dirname(HERE)
 SECRET_KEY = 'fake-key'
 
 INSTALLED_APPS = [
-    # "django.contrib.auth",
     "django.contrib.contenttypes",
     "tests",
 ]
 
+# is this necessary
 MIDDLEWARE_CLASSES = [
-    # 'django.middleware.security.SecurityMiddleware',
-    # 'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
-    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
-    # 'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    # 'django.contrib.messages.middleware.MessageMiddleware',
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
-        'TEST': {
-            'NAME': 'test_db.sqlite3'
-        }
+        'NAME': 'db.sqlite3'
     }
 }
 
