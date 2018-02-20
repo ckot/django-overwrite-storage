31d72aa717de99ce7be439969a810110ca23d508
Scott Silliman <ssilliman@pitt.edu>
2018-02-20 03:38:05 +0000

    renabled the ability to pass location= to overrride MEDIA_ROOT

--- overwrite_storage/storage.py
+++ overwrite_storage/storage.py
@@ -1,15 +1,15 @@
 from django.core.files.storage import FileSystemStorage
-# from django.conf import settings
+from django.conf import settings
 import os
 
 
 class OverwriteStorage(FileSystemStorage):
     """Subclass of FileSystemStorage which clobbers existing file"""
 
-    # def __init__(self, *args, **kwargs):
-    #     """defaults to self.MEDIA_ROOT if location is not passed in"""
-    #     self.location = kwargs.pop("location", settings.MEDIA_ROOT)
-    #     super(OverwriteStorage, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        """defaults to self.MEDIA_ROOT if location is not passed in"""
+        self.location = kwargs.pop("location", settings.MEDIA_ROOT)
+        super(OverwriteStorage, self).__init__(*args, **kwargs)
 
     def get_available_name(self, name, max_length=None):
         """deletes pre-existing file with same name
